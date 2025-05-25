33955e32d1a8f57b23e9780ef13fe6c02e2183c3
Veronika <nikus.kovalcikova@gmail.com>
2025-05-25 13:59:42 +0200

    third commit

--- src/main/java/org/example/SdaTest.java
+++ src/main/java/org/example/SdaTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,8 +22,15 @@ public class SdaTest {
     public void sdaTest(){
         // najprv si nacitame stranku tutorialspoint
         driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        // toto mi zvacsi stranku
+        driver.manage().window().setSize(new Dimension(1936, 1048));
+
         // toto mi najde na stranke prvok a vypíše meno
         driver.findElement(By.id("name")).sendKeys("Veronika");
+        // toto mi najde na stranke prvok a vpise mail
+        driver.findElement(By.id("email")).sendKeys("nikus.kovalcikova@email.com");
+
+
 
     }
 
